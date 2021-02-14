10fa2e2a44d2c5551a46031b50b20e02d5c1fa18
Dumir <Dumir@DESKTOP-U7A86OG>
2021-02-14 16:12:56 +0900

    로그인 화면 생성

---
+++ src/main/java/egovframework/cccdms/login/service/CccdmsLoginVO.java
@@ -0,0 +1,126 @@
+package egovframework.cccdms.login.service;
+
+import java.io.Serializable;
+
+/**
+ * @Class Name : CccdmsLoginVO.java
+ * @Description : Login VO class
+ * @Modification Information
+ * @
+ * @  수정일         수정자                   수정내용
+ * @ -------    --------    ---------------------------
+ * @ 2009.03.03    박지욱          최초 생성
+ * @ 2021.02.14  hong      프로젝트환경에 맞게 커스트마이징버전 생성
+ *
+ *  @author 공통서비스 개발팀 박지욱
+ *  @since 2009.03.03
+ *  @version 1.0
+ *  @see
+ *  
+ */
+public class CccdmsLoginVO implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8274004534207618049L;
+	
+	/** 아이디 */
+	private String id;
+	/** 이름 */
+	private String userName;
+	/** 비밀번호 */
+	private String password;
+	/** 비밀번호 힌트 */
+	private String passwordHint;
+	/** 비밀번호 정답 */
+	private String passwordCnsr;
+	/** 사용자구분 */
+	private String userLevel;
+	/** 사용자구분 */
+	private String userGroup;
+	/** 사용자 IP정보 */
+	private String userIp;
+	/** 마지막로그인일자 */
+	private String lastLoginDt;
+	/** 마지막로그인Ip */
+	private String lastLoginIp;
+	/** 로그인실패횟수 */
+	private String failLoginCnt;
+	/** 가입일자 */
+	private String regDt;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPasswordHint() {
+		return passwordHint;
+	}
+	public void setPasswordHint(String passwordHint) {
+		this.passwordHint = passwordHint;
+	}
+	public String getPasswordCnsr() {
+		return passwordCnsr;
+	}
+	public void setPasswordCnsr(String passwordCnsr) {
+		this.passwordCnsr = passwordCnsr;
+	}
+	public String getUserLevel() {
+		return userLevel;
+	}
+	public void setUserLevel(String userLevel) {
+		this.userLevel = userLevel;
+	}
+	public String getUserGroup() {
+		return userGroup;
+	}
+	public void setUserGroup(String userGroup) {
+		this.userGroup = userGroup;
+	}
+	public String getUserIp() {
+		return userIp;
+	}
+	public void setUserIp(String userIp) {
+		this.userIp = userIp;
+	}
+	public String getLastLoginDt() {
+		return lastLoginDt;
+	}
+	public void setLastLoginDt(String lastLoginDt) {
+		this.lastLoginDt = lastLoginDt;
+	}
+	public String getLastLoginIp() {
+		return lastLoginIp;
+	}
+	public void setLastLoginIp(String lastLoginIp) {
+		this.lastLoginIp = lastLoginIp;
+	}
+	public String getFailLoginCnt() {
+		return failLoginCnt;
+	}
+	public void setFailLoginCnt(String failLoginCnt) {
+		this.failLoginCnt = failLoginCnt;
+	}
+	public String getRegDt() {
+		return regDt;
+	}
+	public void setRegDt(String regDt) {
+		this.regDt = regDt;
+	}
+	
+}
---
+++ src/main/java/egovframework/cccdms/login/web/cccdmsLoginController.java
@@ -0,0 +1,108 @@
+package egovframework.cccdms.login.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+
+import egovframework.cccdms.login.service.CccdmsLoginService;
+import egovframework.cccdms.login.service.CccdmsLoginVO;
+import egovframework.com.cmm.EgovMessageSource;
+import egovframework.com.cmm.util.EgovUserDetailsHelper;
+import egovframework.rte.fdl.cmmn.trace.LeaveaTrace;
+import egovframework.rte.fdl.property.EgovPropertyService;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * CCCDMS 로그인을 화면 컨드롤러
+ * Created by hong on 2021-02-14
+ */
+@Controller
+public class cccdmsLoginController {
+
+	/** EgovLoginService */
+	@Resource(name = "cccdmsLoginService")
+	private CccdmsLoginService cccdmsLoginService;
+
+	/** EgovMessageSource */
+	@Resource(name = "egovMessageSource")
+	EgovMessageSource egovMessageSource;
+
+	/** EgovPropertyService */
+	@Resource(name = "propertiesService")
+	protected EgovPropertyService propertiesService;
+
+	/** TRACE */
+	@Resource(name = "leaveaTrace")
+	LeaveaTrace leaveaTrace;	
+
+	//로그인페이지 호출
+	@RequestMapping(value = "/cccdms/login/loginPage.do")
+	public String getMainPage(HttpServletRequest request, ModelMap model)
+	throws Exception{
+
+		return "main/CccdmsLogin";
+	}
+
+	/**
+	 * 일반 로그인을 처리한다
+	 * @param vo - 아이디, 비밀번호가 담긴 LoginVO
+	 * @param request - 세션처리를 위한 HttpServletRequest
+	 * @return result - 로그인결과(세션정보)
+	 * @exception Exception
+	 */
+	@RequestMapping(value = "/cccdms/login/actionLogin.do")
+	public String actionLogin(@ModelAttribute("cccdmsloginVO") CccdmsLoginVO loginVO, HttpServletRequest request, ModelMap model) throws Exception {
+
+		// 1. 일반 로그인 처리
+		CccdmsLoginVO resultVO = cccdmsLoginService.actionLogin(loginVO);
+
+		boolean loginPolicyYn = true;
+
+		if (resultVO != null && resultVO.getId() != null && !resultVO.getId().equals("") && loginPolicyYn) {
+			request.getSession().setAttribute("LoginVO", resultVO);
+			return "forward:/cccdms/main/mainPage.do";
+		} else {
+			model.addAttribute("message", egovMessageSource.getMessage("fail.common.login"));
+			return "main/CccdmsLogin";
+		}
+
+	}
+
+	/**
+	 * 로그인 후 메인화면으로 들어간다
+	 * @param
+	 * @return 로그인 페이지
+	 * @exception Exception
+	 */
+	@RequestMapping(value = "/cccdms/login/actionMain.do")
+	public String actionMain(ModelMap model) throws Exception {
+
+		// 1. 사용자 인증 처리
+		Boolean isAuthenticated = EgovUserDetailsHelper.isAuthenticated();
+		if (!isAuthenticated) {
+			model.addAttribute("message", egovMessageSource.getMessage("fail.common.login"));
+			return "main/CccdmsLogin";
+		}
+
+		// 2. 메인 페이지 이동
+		return "forward:/cccdms/main/mainPage.do";
+	}
+
+	/**
+	 * 로그아웃한다.
+	 * @return String
+	 * @exception Exception
+	 */
+	@RequestMapping(value = "/cccdms/login/actionLogout.do")
+	public String actionLogout(HttpServletRequest request, ModelMap model) throws Exception {
+
+		RequestContextHolder.getRequestAttributes().removeAttribute("LoginVO", RequestAttributes.SCOPE_SESSION);
+
+		return "forward:/cccdms/login/loginPage.do";
+	}
+}
